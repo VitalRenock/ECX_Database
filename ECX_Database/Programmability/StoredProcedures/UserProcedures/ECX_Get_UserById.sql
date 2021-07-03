eae71a3309e959a34bb7d6a61860e853354d6382
Brigode <renaud.brigode@gmail.com>
2021-07-03 23:56:14 +0200

    Add GetUserById

---
+++ ECX_Database/Programmability/StoredProcedures/UserProcedures/ECX_Get_UserById.sql
@@ -0,0 +1,18 @@
+﻿CREATE PROCEDURE [dbo].[ECX_Get_UserById]
+	@userId INT
+AS
+BEGIN
+
+	SELECT 
+		U.[ID], 
+		U.[Email], 
+		U.[Nickname], 
+		U.[Lastname], 
+		U.[Firstname], 
+		R.[Name]
+	FROM [User] U
+	LEFT JOIN [Role] R ON U.Role_ID = R.ID
+	WHERE U.[ID] = @userId
+	AND U.[Enable] = 1;
+
+END
